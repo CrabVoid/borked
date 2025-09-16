3eb45b2de1f443db72b55914aa49ffb7439cd436
Crab_Void <Ralfs.Egle28@proton.me>
2025-09-16 13:13:38 +0300

    added indexed array and some other stuff rn

---
+++ ahbar.php
@@ -0,0 +1,14 @@
+<?php 
+
+$task = ["Find the princess", "Defeat the bowser", "Save the kingdom"];
+
+while (true) {
+    echo "----------------------------------\n";
+    $text = readline("the princess is in another castle! ");
+    //if ($text == "leave") {
+    //    echo "----------------------------------\n";
+    //    echo "you left!\n";
+    //    echo "----------------------------------\n";
+    //    break;
+    //}
+}
